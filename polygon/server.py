3db9f26736395088268a8ecda4fe9d5f262e01bf
David Castillo <vikruzdavid@gmail.com>
2024-06-07 01:47:13 +0000

    Fix Port

--- polygon/server.py
+++ polygon/server.py
@@ -6,7 +6,7 @@ def main():
     # if production mode is on
     if os.getenv("FLASK_ENV") == "production":
         # run the app gunicorn
-        app.run(host="0.0.0.0", port=5000, debug=False)
+        app.run(host="0.0.0.0", port=5001, debug=False)
     else:
         # run the app flask
         app.run(host="0.0.0.0", port=5000, debug=True)
